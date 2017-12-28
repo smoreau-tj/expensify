47ca41da37cf898aa31b1b6345e26e426a86c6c3
serge-tj <serge@tommyjohn.com>
2017-12-28 17:04:56 -0500

    Worked on sortby

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -121,6 +121,12 @@ const filterReducer = (state = filterReducerDefaultState, action) => {
 
          
          return startDateMatch && endDateMatch && textMatch;
+     }).sort((a, b) => {
+        if(sortBy === 'date'){
+            return a.createdAt < b.createdAt ? 1 : -1 
+        } else if ( sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1
+        }
      })
  }
 
@@ -136,13 +142,13 @@ store.subscribe(() => {
     console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch(addExpense({description: 'Rent', amount:100, createdAt: 1000}))
-const expenseTwo = store.dispatch(addExpense({description: 'Rent2', amount:100,createdAt: -1000}))
+const expenseOne = store.dispatch(addExpense({description: 'Rent', amount:100, createdAt: -21000}))
+const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amount:300,createdAt: -1000}))
 // store.dispatch(addExpense({description: 'Coffee', amount:400}))
 // store.dispatch(addExpense({description: 'Games', amount:5000}))
 // store.dispatch(removeExpense({id: expenseOne.expense.id}))
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 5000}));
- store.dispatch(setTextFilter('rent'));
+ //store.dispatch(setTextFilter('rent'));
 // store.dispatch(setTextFilter());
 // store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
